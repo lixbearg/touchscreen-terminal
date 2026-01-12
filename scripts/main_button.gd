5c31869b8a0b47abe96b5cc69ae4712dad42f9d1
Gabriel Librelon <gabriellibrelon@gmail.com>
2026-01-12 19:27:32 -0300

    add osc messages test settings

--- scripts/main_button.gd
+++ scripts/main_button.gd
@@ -16,10 +16,10 @@ enum ButtonType {MAP, SHIELDS, ALARM}
 func _on_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		osc_client.send_message(on_message_address, [1])
-		osc_client.send_message(off_message_address, [0])
+		#osc_client.send_message(off_message_address, [0])
 		sfx_enable.play()
 	else:
-		osc_client.send_message(on_message_address, [0])
+		#osc_client.send_message(on_message_address, [0])
 		osc_client.send_message(off_message_address, [1])
 		sfx_disable.play()
 	main_button_pressed.emit(button_type, self.get_index(), toggled_on)
