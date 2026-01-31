87c283f3b9c81c37ff3c379586b44d8a936fd0ba
Gabriel Librelon <gabriellibrelon@gmail.com>
2026-01-31 00:43:32 -0300

    update map with actual locations

--- scripts/strings.gd
+++ scripts/strings.gd
@@ -5,9 +5,9 @@ const INFORMATIONS = {
 	0: '- CORREDOR DE ENTRADA -
 	Seja bem vindo à nossa festa! Aqui é onde a aventura começa. Este mapa irá te mostrar tudo que te espera adiante.' ,
 	1: '- PONTE DE COMANDO -
-	Aqui está o controle central da nave. Assuma o comando, explore planetas e treine sua pontaria com os canhões.' ,
+	Aqui está o controle central da nave. Assuma o comando, viaje pelas estrelas e treine sua pontaria com os canhões.' ,
 	2: '- SALÃO PRINCIPAL -
-	A sala do bolo está preparada a grande celebração dos nosso aniversariantes. Não esqueça dos docinhos!' ,
-	3: '- PISTA DE DANÇA -
-	O DJ da nave está garantindo que a música nunca pare. Além disso, os jovens padawans podem sua habilidade com a Força!' 
+	A sala do bolo está preparada a grande celebração dos nossos aniversariantes. Não esqueça dos docinhos!' ,
+	3: '- DIVERSÕES -
+	As melhores atrações da galáxia estão aqui. Muita música, comida, bebida e vários brinquedos para animar sua festa. ' 
 }
